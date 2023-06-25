e209c0f6090c2bb5e06487cdda9eb6cb9d7db30d
KaueMiziara <kauemiziara@gmail.com>
2023-06-25 20:01:39 -0300

    Added Sensor model

---
+++ Models/Sensor.cs
@@ -0,0 +1,9 @@
+namespace SensorList.Models;
+
+public class Sensor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Amount { get; set; }
+}
