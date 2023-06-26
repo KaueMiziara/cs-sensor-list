5d87076ed683a96f13d2d1e263067b99f69054dc
KaueMiziara <kauemiziara@gmail.com>
2023-06-26 15:17:38 -0300

    Connected MainWindowViewModel to Sensor repository

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using SensorList.DAL;
 using SensorList.ViewModels;
 using SensorList.Views;
 
@@ -19,7 +20,7 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = new MainWindowViewModel(new SensorRepository()),
             };
         }
 
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,32 @@
-﻿namespace SensorList.ViewModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ReactiveUI;
+using SensorList.DAL;
+
+namespace SensorList.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private readonly ISensorRepository _sensorRepository;
+    private List<SensorItemViewModel> _sensors;
+
+    public List<SensorItemViewModel> Sensors
+    {
+        get => _sensors;
+        set => this.RaiseAndSetIfChanged(ref _sensors, value);
+    }
+
+    public MainWindowViewModel(ISensorRepository sensorRepository)
+    {
+        _sensorRepository = sensorRepository;
+        LoadSensors();
+    }
+
+    private void LoadSensors()
+    {
+        var sensors = _sensorRepository.GetAllSensors();
+        Sensors = new List<SensorItemViewModel>(
+            sensors.Select(sensor => new SensorItemViewModel(sensor))
+            );
+    }
 }
